424e6e61c22b1658422daa0d3fa238936ec0a3d9
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 17:58:10 +0100

    Push Update

---
+++ com/krzem/wierd_2d_game/Vec2.java
@@ -0,0 +1,38 @@
+package com.krzem.wierd_2d_game;
+
+
+
+public class Vec2{
+	double x,y;
+
+
+
+	public Vec2(double x,double y){
+		this.x=x;
+		this.y=y;
+	}
+
+
+
+	public boolean eq(double ox,double oy){
+		return (this.x==ox&&this.y==oy);
+	}
+
+
+
+	public void add(Vec2 o){
+		this.x+=o.x;
+		this.y+=o.y;
+	}
+	public void add(int ox,int oy){
+		this.x+=ox;
+		this.y+=oy;
+	}
+
+
+
+	public void lerp(Vec2 o,double a){
+		this.x=this.x*(1-a)+o.x*a;
+		this.y=this.y*(1-a)+o.y*a;
+	}
+}
