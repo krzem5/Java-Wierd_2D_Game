f37733bf3384ad3c3d3e207ac9d1fd3e98c17b85
Krzem <krzem5.dev@gmail.com>
2021-04-10 21:16:41 +0200

    Push Update 04/10/2021, 21:16:29

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/wierd_2d_game/Main.java&&jar cvmf ../manifest.mf ../build/wierd_2d_game.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="wierd_2d_game.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/wierd_2d_game.jar
-:end
+python build.py %*
